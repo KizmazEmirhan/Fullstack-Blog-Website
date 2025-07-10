b445213c1743d89dc0368c79202d9ce6824d71fa
Emirhan Kızmaz <133758059+KizmazEmirhan@users.noreply.github.com>
2025-07-10 14:03:18 +0300

    Update AdminPageHeader.vue

--- blogSiteProject/admin-site/adminSite/src/components/UI/AdminPageHeader.vue
+++ blogSiteProject/admin-site/adminSite/src/components/UI/AdminPageHeader.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="bg-white flex container pt-4">
     <div id="brand_name" class="">
-      <img src="../../assets/blogIstanbulSvg.svg" class="w-1/2" alt="" />
+      <img :src="logoImage " class="w-1/2" alt="" />
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      logoImage: new URL('@/assets/BlogIstanbulSvg.svg', import.meta.url).href,
+    }
+  },
+}
+</script>
